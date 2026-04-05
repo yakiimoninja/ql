776050a67b3ac6ad53231c2e886c25ac0a219ba9
yakiimoninja <github@pns.slmail.me>
2026-04-05 22:16:53 +0000

    .

---
+++ README.md
@@ -0,0 +1,8 @@
+[Guide](https://steamcommunity.com/sharedfiles/filedetails/?id=542966946)
+
+Packages needed for archlinux (enable multilib)
+- basedevel
+- lib32-dbus
+- lib32-gcc-libs
+- lib32-glibc
+- lib32-sdl2-compat
---
+++ ql.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+export qPathToStartScript="~/.steam/steamcmd/steamapps/common/qlds/run_server_x86.sh"
+
+gameport=27960
+rconport=28960
+
+echo "========== QuakeStart has started. =========="
+echo "========= $(date) ========="
+cd ~/.steam/steamcmd/steamapps/common/qlds/baseq3
+
+exec $qPathToStartScript \
+	+set g_password "" \
+	+set net_port $gameport \
+	+set zmq_rcon_enable 1 \
+	+set zmq_rcon_password "password" \
+	+set zmq_rcon_port $rconport \
+	+set zmq_stats_enable 1 \
+	+set zmq_stats_password "password" \
+	+set zmq_stats_port $gameport \
+	+set serverstartup "map thunderstruck 3ntffa" \
+	+set sv_mapPoolFile "mappools/mappool_3ntffa.txt" \
